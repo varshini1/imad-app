cd7f20a1f9379debe8f858525b858e32f941e683
varshini1 <31032592+varshini1@users.noreply.github.com>
2017-08-20 11:32:54 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,6 +9,18 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+app.get('/article-one',function(req,res){
+   res.send('article one requsted and will be served here');
+});
+
+app.get('/article-two',function(req,res){
+    res.send('article two requsted and will be served here');
+});
+
+app.get('/article-three',function(req,res){
+    res.send('article three requsted and will be served here');
+});
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
